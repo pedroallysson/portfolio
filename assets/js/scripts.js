d37a8107ef513a74c42abc43208505799883c3b9
Pedro <pedro.alysson@hotmail.com>
2024-12-12 19:17:23 -0300

    added about and contact section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,3 +13,24 @@ function changeTheme(){
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", ()=> {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
+
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach(item =>{
+    item.addEventListener("click", ()=>{
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
